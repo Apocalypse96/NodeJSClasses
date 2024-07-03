97cd4fbcdb92068cd82dcddc8d9bbc3d34b845bd
Aayush Khopade <khopadeayush96@gmail.com>
2024-07-03 10:23:18 +0530

    Learnt PUSH

--- ExpressJs/index.js
+++ ExpressJs/index.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 let courses = [
     { id: 1, name: 'java' },
     { id: 2, name: 'javascript' },
-    { id: 3, name: 'python' }
+    { id: 3, name: 'python' },
+   
 ];
 
 app.get('/courses', function(req, res) {
     res.json(courses);
 });
+
+
+app.post('/courses', function(req, res) {
+    console.log(req.body);
+    let course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+});
+
 
 app.listen(3000, () => {
     console.log('Server is running at http://localhost:3000');
