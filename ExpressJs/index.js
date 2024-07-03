46f86ec8216fc4bbd0a0d0dea50f5569c9eab6bc
Aayush Khopade <khopadeayush96@gmail.com>
2024-07-03 12:01:05 +0530

    Add file read and write functionality using Express

--- ExpressJs/index.js
+++ ExpressJs/index.js
@@ -1,66 +1,90 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+const path = require('path');
+
+const dataFilePath = path.join(__dirname, 'courses.json');
+
 app.use(express.json());
-app.use(loggermiddleware);
+app.use(logger);
+app.use(middleware);
 
-let courses = [
-    { id: 1, name: 'java' },
-    { id: 2, name: 'javascript' },
-    { id: 3, name: 'python' },
-   
-];
+function readCourses() {
+    try {
+        const data = fs.readFileSync(dataFilePath, 'utf8');
+        return JSON.parse(data);
+    } catch (error) {
+        console.error('Error reading courses data:', error);
+        return [];
+    }
+}
 
-app.get('/courses', function(req, res) {
+function writeCourses(courses) {
+    try {
+        fs.writeFileSync(dataFilePath, JSON.stringify(courses, null, 2), 'utf8');
+    } catch (error) {
+        console.error('Error writing courses data:', error);
+    }
+}
+
+app.get('/courses', (req, res) => {
+    const courses = readCourses();
     res.json(courses);
 });
 
-
-app.post('/courses', function(req, res) {
-    console.log(req.body);
-    let course = {
-        id: courses.length + 1,
-        name: req.body.name
-    };
-    courses.push(course);
-    res.send(course);
+app.post('/courses', (req, res) => {
+    const courses = readCourses();
+    let newCourse = { id: courses.length + 1, name: req.body.name };
+    courses.push(newCourse);
+    writeCourses(courses);
+    res.send(courses);
 });
 
-app.put('/courses/:id', function(req, res) {
-    let course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) {
-        return res.status(404).send('The course with the given ID was not found!');
+app.put('/courses/:id', (req, res) => {
+    try {
+        let courses = readCourses();
+        let singleCourse = courses.find(course => course.id === parseInt(req.params.id));
+        if (!singleCourse) {
+            res.status(404).send('Course does not exist');
+            return;
+        }
+        singleCourse.name = req.body.name;
+        writeCourses(courses);
+        res.send(singleCourse);
+    } catch (err) {
+        res.status(500).send(err);
     }
-    course.name = req.body.name;
-    res.send(course);
 });
 
-app.delete('/courses/:id', function(req, res) {
-    let course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) {
-        return res.status(404).send('The course with the given ID was not found!');
+app.delete('/courses', (req, res) => {
+    const courseId = parseInt(req.body.id);
+    let courses = readCourses();
+    const courseIndex = courses.findIndex(c => c.id === courseId);
+
+    if (courseIndex !== -1) {
+        courses.splice(courseIndex, 1);
+        writeCourses(courses);
+        res.json({ data: courses });
+    } else {
+        res.status(404).send('Course not found');
     }
-    let index = courses.indexOf(course);
-    courses.splice(index, 1);
-    res.send(course);
 });
-
 
 function middleware(req, res, next) {
-    console.log("called");
+    console.log("called middleware");
     next();
 }
 
-function loggermiddleware(req, res, next) {
+function logger(req, res, next) {
     const method = req.method;
     const ip = req.ip;
     const hostname = req.hostname;
     const date = new Date().toISOString();
-
-    console.log(`[${date}] ${method} request from ${ip} to ${hostname}`);
+    console.log(`${method} ${ip} ${hostname} ${date}`);
     next();
 }
 
-
-app.listen(3000, () => {
-    console.log('Server is running at http://localhost:3000');
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
 });
